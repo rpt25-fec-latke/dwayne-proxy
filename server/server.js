a94586a32e8d5d197d88b278d8cc12cf1ae650d7
Dwayne Draper <deandraper82@gmail.com>
2021-02-18 23:13:47 -0600

    Changed links for EC2 to work

--- server/server.js
+++ server/server.js
@@ -3,14 +3,21 @@ const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
 const cors = require('cors');
+const axios = require('axios');
 
 app.use(express.static(path.join(__dirname, '../public/')));
 
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.status(200);
-})
+app.get('/metadata', async (req, res) => {
+  const id = req.query.id;
+  try {
+    const { data } = await axios.get(`http://3.131.140.35:3005/metadata/?id=${id}`);
+    res.send(data);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
 
 app.listen(3000, () => {
   console.log('Listening on port 3000.')
