80a7c23c9850386482b23f8ccd261190a1b7ab1f
Dwayne Draper <deandraper82@gmail.com>
2021-02-18 23:23:31 -0600

    Changing things

--- server/server.js
+++ server/server.js
@@ -18,6 +18,25 @@ app.get('/metadata', async (req, res) => {
     res.status(500).send(err);
   }
 });
+app.get('/reviews', async (req, res) => {
+  const id = req.query.id;
+  try {
+    const { data } = await axios.get(`http://204.236.178.72:3001/reviews/?id=${id}`);
+    res.send(data);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+app.get('/reviews', async (req, res) => {
+  const id = req.query.id;
+  try {
+    const { data } = await axios.get(`http://3.137.75.100:3008/game_carousel_info/?id=${id}`);
+    res.json(data);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
 
 app.listen(3000, () => {
   console.log('Listening on port 3000.')
